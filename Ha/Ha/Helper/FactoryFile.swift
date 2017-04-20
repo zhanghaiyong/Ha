6297f0711d9bdfa8dbd03033a329fbcf683e4b56
zhanghaiyong <973490661@qq.com>
2017-04-20 10:51:10 +0800

    框架

---
+++ Ha/Ha/Helper/FactoryFile.swift
@@ -0,0 +1,16 @@
+//
+//  FactoryFile.swift
+//  Ha
+//
+//  Created by zhy on 2017/4/19.
+//  Copyright © 2017年 zhanghaiyong. All rights reserved.
+//
+
+
+
+import UIKit
+
+class GeneralFactory: NSObject {
+
+    
+}
--- Ha/Ha/bridging.h
+++ Ha/Ha/bridging.h
@@ -1,13 +1,5 @@
-//
-//  bridging.h
-//  Ha
-//
-//  Created by zhy on 2017/4/14.
-//  Copyright © 2017年 zhanghaiyong. All rights reserved.
-//
-
-#ifndef bridging_h
-#define bridging_h
 
 
-#endif /* bridging_h */
+
+
+#import "AFNetworking.h"
